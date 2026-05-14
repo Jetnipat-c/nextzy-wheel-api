2ba55291f3905222d5195e01df7c580c0d538b35
Leo <dev.jetnipat@gmail.com>
2026-05-14 21:48:34 +0700

    chore: configuration module

---
+++ src/infrastructure/infrastructure.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { configuration } from './config/configuration';
+
+@Module({
+  imports: [ConfigModule.forRoot({ load: [configuration], isGlobal: true })],
+})
+export class InfrastructureModule {}
