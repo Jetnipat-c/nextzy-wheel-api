0cae815c9dce7560cae104585ffd9b534f3db036
Leo <dev.jetnipat@gmail.com>
2026-05-14 21:50:53 +0700

    chore: initialize Prisma ORM infrastructure and define initial database schema

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -3,7 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 
 import { configuration } from './config/configuration';
 
+import { PrismaModule } from './database/prisma/prisma.module';
+
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration], isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+    PrismaModule,
+  ],
 })
 export class InfrastructureModule {}
